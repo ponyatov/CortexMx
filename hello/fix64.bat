b2c6c8d9ae72dc72b90820093d9b688dbe01c1a6
Dmitry Ponyatov <dponyatov@gmail.com>
2014-07-14 13:24:35 +0400

    * исправлена проблема с make на Windows x64
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hello/fix64.bat
+++
@@ -1,1 +0,0 @@
-gcc -o fix64.exe fix64.c
--- hello/fix64.c
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	char buf[1024];
-	sprintf(buf,"arm-none-eabi-objdump %s %s > %s.dump\n",argv[1],argv[2],argv[2]);
-	return system(buf);
-}
