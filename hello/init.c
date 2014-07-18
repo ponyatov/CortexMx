0c034baec33a9e88f2fee815aa6022830a670c65
Dmitry Ponyatov <dponyatov@gmail.com>
2014-07-19 02:37:39 +0400

    линкер-скрипты переведены на схему -T $(CPU) -T generic.ld для использования из других подкаталогов типа /book/blablabla
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hello/init.c
+++ hello/init.c
@@ -1,15 +1,24 @@
 
 // пример как преобразовать макроопределение в строку
-#define QUOTE1(Y) #Y
-#define QUOTE(Y) QUOTE1(Y)
+#define QUOTEQUOTE(Y) #Y
+#define QUOTE(Y) QUOTEQUOTE(Y)
 
 // сопи направо
 __attribute__ ((section(".copyright")))
 char _C_init[]=QUOTE(CPU) " init (c) Azbuka ARMaturschika";
 
-#if CPU==LM3S811
-#include "LM3S811.h"
+// пример условной компиляции по значению макроса, заданного
+// в командной строке запуска компилятора -DCPU=$(CPU)
+#if CPU==LM3S81122
+	#include "LM3S811.h"
+#else
+	// пример как аварийно остановить компиляцию
+	#pragma message QUOTE(CPU)
+	#error CPU not supported
 #endif
+
+//#pragma message QUOTE(CPU)
+//#error not supported
 
 // точка входа
 init () {}
